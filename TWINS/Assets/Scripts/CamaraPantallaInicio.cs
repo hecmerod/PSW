3323e99c108f040819ba64fa23a45f90e5ddfbd3
hecmerod <47757085+hecmerod@users.noreply.github.com>
2020-04-13 12:35:53 +0200

    pantalla Inicio
    he hecho una mierda

---
+++ TWINS/Assets/Scripts/CamaraPantallaInicio.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraPantallaInicio : MonoBehaviour
+{
+    [SerializeField] private GameObject cube;
+    void Update()
+    {
+        gameObject.transform.LookAt(cube.transform);
+        transform.Translate(Vector3.right * 2 * Time.deltaTime);
+    }
+}

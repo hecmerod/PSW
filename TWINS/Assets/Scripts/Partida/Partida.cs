f974a31c7b102e2762f213127d56630e765c9415
jinyeji <jinyeji@users.noreply.github.com>
2020-04-16 21:20:04 +0200

    Cambiado tiempo de 1 a 1000

--- TWINS/Assets/Scripts/Partida/Partida.cs
+++ TWINS/Assets/Scripts/Partida/Partida.cs
@@ -19,7 +19,7 @@ public abstract class Partida : MonoBehaviour
     protected int turno = 0, pairsFound = 0, numCardsTurned = 0;
 
     protected bool startedTimer = false;
-    protected float time = 1, timePlayed = 0;
+    protected float time = 1000, timePlayed = 0;
 
     protected void Awake() {
         CargarRecursos();

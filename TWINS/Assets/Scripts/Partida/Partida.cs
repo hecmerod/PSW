ddb76d2561d1338e893fc7d0c41f0306111e90ca
TheCraxo <62883984+TheCraxo@users.noreply.github.com>
2020-04-20 14:38:41 +0200

    Categoria base

--- TWINS/Assets/Scripts/Partida/Partida.cs
+++ TWINS/Assets/Scripts/Partida/Partida.cs
@@ -25,7 +25,7 @@ public abstract class Partida : MonoBehaviour
         CargarRecursos();
         InstanciarAnimacion();
         tematica = ElegirBarajaAPArtida.tematica;
-        tamaño = "grande";
+        tamaño = "pequeño";
     }
 
     protected void Update() {

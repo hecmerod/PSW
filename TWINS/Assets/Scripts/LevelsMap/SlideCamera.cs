949aa5b19002bd73d24f78ca379665bf78d3a8b4
hecmerod <47757085+hecmerod@users.noreply.github.com>
2020-05-01 15:23:12 +0200

    MapaNiveles

---
+++ TWINS/Assets/Scripts/LevelsMap/SlideCamera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SlideCamera : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Camera camera;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        print("OnMouseEnter");
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        print("OnMouseExit");
+    }
+}

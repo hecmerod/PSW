47a73c4dbfee3290fab166d6c58baa9517cb52c6
TheCraxo <62883984+TheCraxo@users.noreply.github.com>
2020-05-03 18:02:47 +0200

    Pausar
    Mas bonito mas opciones

---
+++ TWINS/Assets/Scripts/MenuPausa.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPausa : MonoBehaviour
+{
+    public static bool Pausado = false;
+    public static bool checker = false;
+    public GameObject menuPausaUI;
+    public GameObject salirPreguntaUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (checker == false)
+            {
+                if (Pausado)
+                {
+                    Reanudar();
+                }
+                else
+                {
+                    Pausar();
+                }
+            }
+            else
+            {
+                vuelvoDeQuitar();
+            }
+        }
+    }
+    public void Reanudar()
+    {
+        menuPausaUI.SetActive(false);
+        Time.timeScale = 1f;
+        Pausado = false;
+    }
+    void Pausar()
+    {
+        menuPausaUI.SetActive(true);
+        Time.timeScale = 0f;
+        Pausado = true;
+    }
+    public void cargarOpciones()
+    {
+        //Sprint 3
+    }
+    public void quitarPartida()
+    {
+        menuPausaUI.SetActive(false);
+        salirPreguntaUI.SetActive(true);
+        checker = true;
+    }
+    public void quitarDefinitivo()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("PantallaInicio");
+    }
+    public void vuelvoDeQuitar()
+    {
+        menuPausaUI.SetActive(true);
+        salirPreguntaUI.SetActive(false);
+        checker = false;
+    }
+}

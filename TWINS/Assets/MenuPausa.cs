08c74af0eb6fde25f293690bf632b1996f759d5c
TheCraxo <62883984+TheCraxo@users.noreply.github.com>
2020-05-03 14:46:42 +0200

    Pausar
    Pausado hecho en partida estandar, falta ponerlo bonito

---
+++ TWINS/Assets/MenuPausa.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuPausa : MonoBehaviour
+{
+    public static bool Pausado = false;
+    public GameObject menuPausaUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Pausado)
+            {
+                Reanudar();
+            }
+            else
+            {
+                Pausar();
+            }
+        }
+    }
+    public void Reanudar()
+    {
+        menuPausaUI.SetActive(false);
+        Time.timeScale = 1f;
+        Pausado = false;
+    }
+    void Pausar()
+    {
+        menuPausaUI.SetActive(true);
+        Time.timeScale = 0f;
+        Pausado = true;
+    }
+    public void cargarOpciones()
+    {
+
+    }
+    public void quitarPartida()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("PantallaInicio");
+    }
+}

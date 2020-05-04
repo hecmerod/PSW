9adf230069db22a084361cbe9432a5d241deb8ae
hecmerod <47757085+hecmerod@users.noreply.github.com>
2020-05-04 14:09:57 +0200

    Mapa Niveles nivelSeleccioando

---
+++ TWINS/Assets/Scripts/LevelsMap/Level.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour
+{
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+    }
+    void OnMouseExit() { transform.localScale = Vector3.one; }
+}

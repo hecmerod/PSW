e763990d9f5e98ee53e8f3243ee29975a2714a0c
jinyeji <jinyeji@users.noreply.github.com>
2020-05-11 22:55:07 +0200

    Tiempo

--- TWINS/Assets/Scripts/Tiempo.cs
+++ TWINS/Assets/Scripts/Tiempo.cs
@@ -2,18 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Tiempo : MonoBehaviour
 {
-    private static Text miTexto;
-
-    void Start()
+    private GameObject canvas;
+    public bool comenzarTiempo = false, partidaTerminada = false;
+    public Text miTiempo, textContador, puntuacion, textPuntuacion;
+    public float time = 60, timePlayed = 0, contador;
+    Partida partida;
+    public void Awake()
     {
-        miTexto = GetComponent<Text>();
+        LoadSetting();
+        time = GameProperties.time;
+        contador = time;
+        textContador.text = "Tiempo: " + time;
+    }
+    public void Update()
+    {
+      Timer();
+    }
+    public void LoadSetting()
+    {
+        canvas = GameObject.Find("Canvas");
+        miTiempo = canvas.transform.GetComponentsInChildren<Text>()[3];
+        textContador = canvas.transform.GetComponentsInChildren<Text>()[0];
+        textPuntuacion = canvas.transform.GetComponentsInChildren<Text>()[2];
+
+        textContador.transform.localPosition = GameProperties.cronoPosition;
+
+    }
+    public void Timer()
+    {
+        if (comenzarTiempo)
+        {
+            timePlayed += Time.deltaTime;
+            contador -= Time.deltaTime;
+            textContador.text = "Tiempo: " + (int)contador;
+        }
+        if(timePlayed >= time)
+        {
+            miTiempo.text = "Tiempo: " + (int)time;
+            if(partida.puntos < 0) { partida.puntos = 0; }
+            textPuntuacion.text = "Puntuación: " + partida.puntos;
+        }
+        if (partidaTerminada)
+        {
+            miTiempo.text = "Tiempo: " + (int)timePlayed;
+            if(partida.puntos < 0) { partida.puntos = 0; }
+            textPuntuacion.text = "Puntuación: " + partida.puntos;
+        }
     }
 
-    public static void SetTiempo(float tiempo)
+    public void InicializarPartida(Partida partida)
     {
-        miTexto.text = ((int)tiempo).ToString();
+        this.partida = partida;
     }
 }

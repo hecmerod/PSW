51590ea9f7fdd9e73a9f6eef4d3a3b67ea70ef31
hecmerod <47757085+hecmerod@users.noreply.github.com>
2020-05-18 12:46:32 +0200

    elegirBaraja

---
+++ TWINS/Assets/Scripts/ElegirBaraja/Elegidas.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elegidas : MonoBehaviour
+{
+    public Vector3[] posiciones = new Vector3[15];
+    private bool[] posOcupadas = new bool[15];
+
+    private void Awake() {
+    }
+}

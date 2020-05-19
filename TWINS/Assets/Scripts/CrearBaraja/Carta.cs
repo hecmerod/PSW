37ae5be3e48b2a4de815ce609e8b6fbae44bd7c0
hecmerod <47757085+hecmerod@users.noreply.github.com>
2020-05-19 12:44:42 +0200

    dragAndDrop

---
+++ TWINS/Assets/Scripts/CrearBaraja/Carta.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carta : MonoBehaviour
+{
+    public Vector3 startPosition, startDistance;
+
+    private Rigidbody rigidbody;
+    private GameObject marco;
+    private bool showMarco = true;
+
+    private bool dragging;
+    private float distance;
+
+    private Elegidas elegidas;
+
+    void Awake()
+    {
+        rigidbody = this.GetComponent<Rigidbody>();
+        marco = this.gameObject.transform.GetChild(1).gameObject;
+
+        elegidas = FindObjectOfType<Elegidas>();
+
+        startPosition = this.transform.position;
+    }
+
+    private void Update() {
+
+        if (dragging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = new Vector3(rayPoint.x, 0.2f, rayPoint.z);
+            
+        }
+    }
+
+    private void OnMouseDown() {
+        dragging = true; showMarco = false;
+
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        dragging = true;
+    }
+
+    private void OnMouseUp() {
+        dragging = false;
+
+        if (Input.mousePosition.x <= Screen.width * 0.27f) {
+            Vector3 aux = elegidas.ReturnPos();
+
+            if ( aux != Vector3.zero) transform.position = aux;
+            else transform.position = startPosition;
+
+        } else transform.position = startPosition;
+        
+    }
+
+    private void OnMouseOver() { marco.SetActive(showMarco); }
+    private void OnMouseExit() { marco.SetActive(false); }
+}
---
+++ TWINS/Assets/Scripts/CrearBaraja/Elegidas.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elegidas : MonoBehaviour
+{
+    public Vector3[] posiciones = new Vector3[15];
+    private bool[] posOcupadas = new bool[15];
+
+    private void Awake() {
+    }
+
+    public Vector3 ReturnPos() {
+        for (int i = 0; i < 15; i++) {
+            if (!posOcupadas[i]) {
+                posOcupadas[i] = true;
+                return posiciones[i];
+            }
+        }
+        return Vector3.zero;
+    }
+}

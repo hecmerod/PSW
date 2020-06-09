ed70934002e0913f0fe513b361cf66b81cbc6586
TheCraxo <62883984+TheCraxo@users.noreply.github.com>
2020-06-09 16:34:37 +0200

    Nada

---
+++ TWINS/Assets/Test/EditMode/PartidaTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class PartidaTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void PartidaTestSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+    }
+}
